fbc1fc31efd8060dfcc7c9e6d799e6747ebfc90f
Tal Malka <tal.coder@gmail.com>
2022-12-01 17:43:13 +0200

    Update

--- my_mat.c
+++ my_mat.c
@@ -1,6 +1,12 @@
 #include "my_mat.h"
 #include <stdio.h>
 
+int min(int *a, int *b){
+    if(a<b)
+        return a;
+    else
+        return b;
+}
 int a[TEN][TEN];
 void ACase(){
     for (int i = 0; i < TEN; i++){
@@ -9,57 +15,34 @@ void ACase(){
         }
     }
 }
-int g=0;
-int count=0;
-int RecB(int x,int y,int prev){
-    if(x==y){
-        return 1;
-    }
+void BCase(int x , int y){
+   for (int i = 0; i < TEN; i++) {
+            for (int j = 0; j < TEN; j++) {
+                if(a[i][j]==0 && i!=j){
+                    a[i][j] = 10000;
+                }
+            }
+
+        }
+        for (int k = 0; k <TEN; k++)
+        {
+            for (int i = 0; i <TEN; i++)
+            {
+                for (int j = 0; j <TEN; j++)
+                {
+                        a[i][j] = min(a[i][j], a[i][k] + a[k][j]);
+
+                }
+
+            }
+
+        }
     if(a[x][y]!=0){
-        return 1;
+        printf("true");
     }
-    if(count==1000){
-        return 0;
+    else{
+        printf("false");
     }
-    if(a[x][g]!=0 && g!=prev){
-            int gbis=g;
-            g=0;
-            count++;
-            return RecB(gbis,y,x);
-    }
-    else {
-        count++;
-        g++;
-        return RecB(x,y,x);
-    }
-    return 0;
-    
 }
-void BCase(){
-    int x;
-    scanf("%d",&x);
-    int y;
-    scanf("%d",&y);
-    if(a[x][y]!=0){
-        printf("true \n");
-        return;
-    }
-    if(x==y){
-        printf("false \n");
-        return;
-    }
-    int b = RecB(x,y,x);
-    if(b==1){
-        printf("true \n");
-        return;
-    }
-    printf("False \n");
-    return;
-    
-}
-int min(int a, int b){
-    if(a<b)
-        return a;
-    else
-        return b;
-}
+
+
